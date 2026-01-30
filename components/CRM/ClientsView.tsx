b60468b5c19e351dfb27709f0e18be7af3ead80a
Developer <dev@praetor.local>
2026-01-30 21:04:36 +0100

    fix: correct StandardTable import path in ClientsView

--- components/CRM/ClientsView.tsx
+++ components/CRM/ClientsView.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useMemo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Client } from '../types';
-import StandardTable, { Column } from './shared/StandardTable';
+import StandardTable, { Column } from '../shared/StandardTable';
 import StatusBadge from './shared/StatusBadge';
 import Modal from './shared/Modal';
 
